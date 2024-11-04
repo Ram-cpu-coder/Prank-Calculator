db763b86b3ac8c9b2c6f8b1a5292af517955672b
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-11-05 00:12:56 +1100

    js started

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Its working.");
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.querySelector(".input");
+
+  const btn = document.querySelectorAll(".button");
+  btn.forEach((buttons) => {
+    buttons.addEventListener("click", () => {
+      const text = input.value;
+      console.log("clicked");
+      console.log(text);
+    });
+  });
+});
