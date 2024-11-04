6eff111edcfe4781ce412c8b3b17eed888afea1b
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-11-05 10:37:21 +1100

    button and the div input managed

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 // ==========================taking input upon clicking the buttons=======
 
-const input = document.querySelector("input");
+const input = document.getElementsByClassName(".input");
 // console.log(input);
 
 const btn = document.querySelectorAll(".button");
+const clearBtn = document.querySelector("ac");
+
 btn.forEach((buttons) => {
   buttons.addEventListener("click", (e) => {
-    let buttonText = e.target.textContent;
-    // console.log(e.target.textContent);
-    input.value += buttonText;
+    let buttonText = e.target.innerText;
+    console.log(buttonText);
+    input.innerText += buttonText;
   });
 });
