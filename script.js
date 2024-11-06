0dddc549b6351b8315f1de711e7f8bb867bc7a74
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-11-07 09:45:47 +1100

    again display function

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
+//first of all we import all the elements that we are going to use here 
 const displayElm = document.querySelector(".input");
 const btns = document.querySelectorAll(".button");
-
 let strToDisplay = "";
 
-
-const calculatorOperation= (val)=>{
- 
-    strToDisplay += val;
-    display(strToDisplay)
-    
-
+const calculatorOperation = (val)=>{
+  strToDisplay += val;
+  display(strToDisplay);
 }
 btns.forEach((button)=>{
-  button.addEventListener("click",()=>{
+  button.addEventListener("click", ()=>{
     const val = button.innerText;
     calculatorOperation(val);
-
   })
 })
 
-const display= (str)=>{
- displayElm.innerText = str || "0.00"; 
+
+const display = (str) =>{
+  displayElm.innerText = str || "0.00";
 }
