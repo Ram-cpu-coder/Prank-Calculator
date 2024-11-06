3ef8fb09e3bb6cbf665ef7fc4d0534b2d59b1d43
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-11-07 09:39:19 +1100

    displaying in the input section

--- script.js
+++ script.js
@@ -1,69 +1,24 @@
 const displayElm = document.querySelector(".input");
 const btns = document.querySelectorAll(".button");
+
 let strToDisplay = "";
-const operators = "%*/-+";
-let lastOperator = "";
-const calculatorOperation = (val) => {
 
-  displayElm.style.background = "";
-  displayElm.classList.remove("prank");
-  if (val === "=") {
-    const lastChar = strToDisplay[strToDisplay.length - 1];
-    if (operators.includes(lastChar)) {
-      strToDisplay = strToDisplay.slice(0, -1);
-    }
-    return total();
-  }
-  if (operators.includes(val)) {
-    const lastChar = strToDisplay[strToDisplay.length - 1];
-    lastOperator = val;
-    if (operators.includes(lastChar)) {
-      strToDisplay = strToDisplay.slice(0, -1);
-    }
-  }
-  if(val === "AC"){
-    strToDisplay = "";
-    display();
-    return;
-  }
-if(val === "C"){
 
-  strToDisplay = strToDisplay.slice(0 , -1);
-  return display(strToDisplay);
+const calculatorOperation= (val)=>{
+ 
+    strToDisplay += val;
+    display(strToDisplay)
+    
+
 }
-  if(val === "."){
-    const indexOfLastOperator = strToDisplay.lastIndexOf(lastOperator);
-    const lastNumberSet = strToDisplay.slice( indexOfLastOperator);
-    if(lastNumberSet.includes(".")) return;
-    if(!lastOperator && strToDisplay.includes(".")) return;
-  }
-  strToDisplay += val;
-  display(strToDisplay);
-};
-
-btns.forEach((button) => {
-  button.addEventListener("click", () => {
+btns.forEach((button)=>{
+  button.addEventListener("click",()=>{
     const val = button.innerText;
     calculatorOperation(val);
-  });
-});
 
-const display = (str) => {
-  displayElm.innerText = str || "0.00";
-};
+  })
+})
 
-const total = () => {
-
-  const extraVal = randomNum();
-  if(extraVal){
-    displayElm.style.background = "red";
-    displayElm.classList.add("prank");
-  }
-  const ttl = eval(strToDisplay) + extraVal;
-  strToDisplay = ttl.toString();
-  display(ttl);
-};
-const randomNum = ()=>{
-  const num = Math.round(Math.random()*10);
- return num < 5 ? num:0;
+const display= (str)=>{
+ displayElm.innerText = str || "0.00"; 
 }
